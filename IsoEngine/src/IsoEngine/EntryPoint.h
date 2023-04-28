bfc817ba1e982701462daa701bc1f99701bde314
JohnMarcus <57123223+mynahcode@users.noreply.github.com>
2023-04-27 23:59:19 -0700

    premake solution and libraries (with License) added to the vendor folder within repository to be used to build/release/debug IsoEngine and premake5.lua file created. Minor formatting/comment fixes in various files and in EntryPoint.h

--- IsoEngine/src/IsoEngine/EntryPoint.h
+++ IsoEngine/src/IsoEngine/EntryPoint.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "IsoEngine/IsoLogger/IsoLogger.h"
-#include <thread>
 
 #ifdef _WIN32
 
@@ -9,16 +8,6 @@ extern IE::Application* IE::CreateApplication();
 
 int main(int argc, char** argv)
 {
-	IE::IELogger::IsoLogger::EnableIsoFileOut("logs/testlog.txt");
-
-	//std::thread threads[10];
-
-	//for (int i = 0; i < 10; i++) { threads[i] = std::thread(log_test, i); }
-	//for (int i = 0; i < 10; i++) { threads[i].join(); }
-
-	const char* name = "User";
-	ISOLOGGER_INFO("Hello %s", name);
-
 	auto app = IE::CreateApplication();
 	app->Run();
 	delete app;
