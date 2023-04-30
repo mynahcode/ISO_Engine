30823230bf99438f821cd10836f39a0acefd601a
JohnMarcus <57123223+mynahcode@users.noreply.github.com>
2023-04-29 23:49:46 -0700

    New event handling system introduced, empty header files uploaded to be implemented. Basic EventType classes introduced.

---
+++ IsoEngine/src/IsoEngine/Events/Event.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "IsoEngine.h"
+
+#include <string>
+#include <functional>
+
+namespace IE
+{
+	
+	enum class EventType
+	{
+		None = 0,
+		WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
+		AppTick, AppUpdate, AppRender,
+		KeyPressed, KeyReleased,
+		MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
+	};
+
+
+}
