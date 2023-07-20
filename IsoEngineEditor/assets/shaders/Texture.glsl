d14441fa90fb57cd6fad55a0e3de9a396aad7231
JohnMarcus <57123223+mynahcode@users.noreply.github.com>
2023-07-19 22:27:01 -0700

    removed z-fighting bug in texture shader

--- IsoEngineEditor/assets/shaders/Texture.glsl
+++ IsoEngineEditor/assets/shaders/Texture.glsl
@@ -1,7 +1,7 @@
 // Texture Shader
 
 #type vertex
-#version 330 core
+#version 450
 			
 layout(location = 0) in vec3 a_Position;
 layout(location = 1) in vec4 a_Color;
@@ -13,7 +13,7 @@ uniform mat4 u_ViewProjection;
 
 out vec4 v_Color;
 out vec2 v_TextureCoord;
-out float v_TextureIndex;
+out flat float v_TextureIndex;
 out float v_TilingFactor;
 
 void main()
@@ -26,13 +26,13 @@ void main()
 }
 
 #type fragment
-#version 330 core
+#version 450
 
 layout(location = 0) out vec4 color;
 
 in vec4 v_Color;
 in vec2 v_TextureCoord;
-in float v_TextureIndex;
+in flat float v_TextureIndex;
 in float v_TilingFactor;
 
 uniform sampler2D u_Textures[32];
