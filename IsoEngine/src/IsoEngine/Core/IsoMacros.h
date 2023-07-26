5f352522f9d0969fb1ea2bf665acc036ca7aa37a
JohnMarcus <57123223+mynahcode@users.noreply.github.com>
2023-07-25 23:57:17 -0700

    bind function macro now uses std::bind

--- IsoEngine/src/IsoEngine/Core/IsoMacros.h
+++ IsoEngine/src/IsoEngine/Core/IsoMacros.h
@@ -47,7 +47,7 @@
 #define BIT(x) (1 << x)
 
 /* Event Binding Function */
-#define IE_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
+#define IE_BIND_EVENT_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
 
 namespace IE
 {
