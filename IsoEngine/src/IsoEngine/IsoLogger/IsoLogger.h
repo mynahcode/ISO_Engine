7ec495d91b9c729adccb19392cea9b2c2b2953d4
JohnMarcus <57123223+mynahcode@users.noreply.github.com>
2023-08-10 23:54:36 -0700

    Early integration of fmt into logger complete for different priority levels, colors of priority levels are subject to change, should refactor logger to not have to use a switch and to support separate log files for IsoEngine, the IsoEngine Editor, and IsoEngine applications.

--- IsoEngine/src/IsoEngine/IsoLogger/IsoLogger.h
+++ IsoEngine/src/IsoEngine/IsoLogger/IsoLogger.h
@@ -36,8 +36,7 @@ namespace IE
 			/* Member Variable Initializations */
 			static IELogger_Priority priority_level;
 			std::mutex logger_mutex;
-			const char* filepath = 0;
-			FILE* file;
+			bool IsoLoggerOut = false;
 
 			IsoLogger() = default;
 			IsoLogger(const IsoLogger&) = delete;							// Copy-Constructor
@@ -51,56 +50,62 @@ namespace IE
 				return iso_logger;
 			}
 
-			void vLog(const char* file, int line, fmt::string_view format, fmt::format_args args)
-			{
-				fmt::print("{}: {}:", file, line);
-				fmt::vprint(format, args);
-			}
-
-			void IsoLog(int line_num, const char* src_file, IELogger_Priority msg_priority, const char* priority, fmt::string_view format, fmt::format_args args)
+			template<typename... T>
+			void IsoLog(int line_num, fmt::string_view src_file, IELogger_Priority msg_priority, const char* priority, fmt::string_view format, fmt::format_args args)
 			{
 				if (priority_level <= msg_priority)
 				{
-
-					std::scoped_lock lock(logger_mutex);
-					std::time_t tp = std::time(nullptr);
-					auto const curr_time = fmt::localtime(tp);
-
-					fmt::print("[{:%F %r}]  ", curr_time);
-					//fmt::print("{}", msg_priority_str);
-					//fmt::print("{}: {}:", file, line_num);
-					fmt::vprint(format, args);
-
-					// Enable File-Logging Check
-					/*
-					if (file)
+					if (!IsoLoggerOut)
 					{
-						fprintf(file, "%.19s %s", buffer, am_pm);
-						fprintf(file, msg_priority_str);
-						fprintf(file, msg, args...);
-						fprintf(file, " On Line %d in %s", line_num, src_file);
-						fprintf(file, "\n");
+						std::scoped_lock lock(logger_mutex);
+						std::time_t tp = std::time(nullptr);
+						auto const curr_time = fmt::localtime(tp);
+
+						fmt::print("[{:%F %r}]  ", curr_time);
+
+						switch (msg_priority)
+						{
+						case IELogger_Priority::TRACE:
+							fmt::print(bg(fmt::color::rebecca_purple), priority);
+							break;
+
+						case IELogger_Priority::DEBUG:
+							fmt::print(bg(fmt::color::royal_blue), priority);
+							break;
+						case IELogger_Priority::INFO:
+							fmt::print(bg(fmt::color::forest_green), priority);
+							break;
+
+						case IELogger_Priority::WARN:
+							fmt::print(bg(fmt::color::dark_golden_rod), priority);
+							break;
+
+						case IELogger_Priority::CRITICAL:
+							fmt::print(bg(fmt::color::crimson), priority);
+							break;
+
+						case IELogger_Priority::FATAL:
+							fmt::print(bg(fmt::color::dark_red), priority);
+							break;
+						}
+
+						fmt::print(" in {} ({:d})  ", src_file, line_num);
+						fmt::vprint(format, args);
 					}
-					*/
+					// TODO: Fix file logging and refactor for supporting unique log files for the engine, the editor, and the application
+					else
+					{
+						std::scoped_lock lock(logger_mutex);
+						//fmt::ostream out = fmt::output_file("./TheGame/logs/log.txt", fmt::file::WRONLY | fmt::file::CREATE | fmt::file::APPEND);
+						FILE* file = std::fopen("log.txt", "w");
+						std::time_t tp = std::time(nullptr);
+						auto const curr_time = fmt::localtime(tp);
+						fmt::print(file, "[{:%F %r}]  ", curr_time);
+						fmt::print(file, " {} in {} ({:d})  ", priority, src_file, line_num);
+						fmt::vprint(file, format, args);
+						std::fclose(file);
+					}
 				}
-			}
-
-			void enable_IsoFileOut()
-			{
-				// TODO: Refactor to account for a file pointer being null cause fclose() error follow: https://stackoverflow.com/questions/34824568/visual-studio-2015-shows-debug-assertion-failed
-				// * Logging w/o file output works for now *
-				/*
-				if (file != 0)
-				{
-					fclose(file);
-				}
-
-				file = fopen(filepath, "a");
-				if (file == 0)
-				{
-					printf("<<IsoLogger>> Failed to open file at %s", filepath);
-				}
-				*/
 			}
 
 			void free_File()
@@ -110,19 +115,9 @@ namespace IE
 			}
 
 		public:
-
-			inline static IELogger_Priority GetPriority() { return priority_level;  }
-
 			inline static void SetPriority(IELogger_Priority priority) 
 			{ 
 				get_InstanceIsoLogger().priority_level = priority; 
-			}
-
-			static void EnableIsoFileOut()
-			{
-				IsoLogger& isologger_instance = get_InstanceIsoLogger();
-				//isologger_instance.filepath = "log.txt";
-				//isologger_instance.enable_IsoFileOut();
 			}
 
 			static void EnableIsoFileOut(const char* new_filepath)
@@ -133,39 +128,39 @@ namespace IE
 			}
 
 			template<typename... T>
-			static void Trace(int line_num, const char* src_file, fmt::format_string<T...> format, T&&... args)
+			static void Trace(int line_num, fmt::format_string<T...> src_file, fmt::format_string<T...> format, T&&... args)
 			{
-				get_InstanceIsoLogger().IsoLog(line_num, src_file, IELogger_Priority::TRACE, "[TRACE]: ", format, fmt::make_format_args(args...));
+				get_InstanceIsoLogger().IsoLog(line_num, src_file, IELogger_Priority::TRACE, "[TRACE]:", format, fmt::make_format_args(args...));
 			}
 
 			template<typename... T>
-			static void Debug(int line_num, const char* src_file, fmt::format_string<T...> format, T&&... args)
+			static void Debug(int line_num, fmt::format_string<T...> src_file, fmt::format_string<T...> format, T&&... args)
 			{
-				get_InstanceIsoLogger().IsoLog(line_num, src_file, IELogger_Priority::DEBUG, "[DEBUG]: ", format, fmt::make_format_args(args...));
+				get_InstanceIsoLogger().IsoLog(line_num, src_file, IELogger_Priority::DEBUG, "[DEBUG]:", format, fmt::make_format_args(args...));
 			}
 
 			template<typename... T>
-			static void Info(int line_num, const char* src_file, fmt::format_string<T...> format, T&&... args)
+			static void Info(int line_num, fmt::format_string<T...> src_file, fmt::format_string<T...> format, T&&... args)
 			{
-				get_InstanceIsoLogger().IsoLog(line_num, src_file, IELogger_Priority::INFO, "[INFO]: ", format, fmt::make_format_args(args...));
+				get_InstanceIsoLogger().IsoLog(line_num, src_file, IELogger_Priority::INFO, "[INFO]:", format, fmt::make_format_args(args...));
 			}
 
 			template<typename... T>
-			static void Warn(int line_num, const char* src_file, fmt::format_string<T...> format, T&&... args)
+			static void Warn(int line_num, fmt::format_string<T...> src_file, fmt::format_string<T...> format, T&&... args)
 			{
-				get_InstanceIsoLogger().IsoLog(line_num, src_file, IELogger_Priority::WARN, "[WARN]: ", format, fmt::make_format_args(args...));
+				get_InstanceIsoLogger().IsoLog(line_num, src_file, IELogger_Priority::WARN, "[WARN]:", format, fmt::make_format_args(args...));
 			}
 
 			template<typename... T>
-			static void Critical(int line_num, const char* src_file, fmt::format_string<T...> format, T&&... args)
+			static void Critical(int line_num, fmt::format_string<T...> src_file, fmt::format_string<T...> format, T&&... args)
 			{
-				get_InstanceIsoLogger().IsoLog(line_num, src_file, IELogger_Priority::CRITICAL, "[CRITICAL]: ", format, fmt::make_format_args(args...));
+				get_InstanceIsoLogger().IsoLog(line_num, src_file, IELogger_Priority::CRITICAL, "[CRITICAL]:", format, fmt::make_format_args(args...));
 			}
 
 			template<typename... T>
-			static void Fatal(int line_num, const char* src_file, fmt::format_string<T...> format, T&&... args)
+			static void Fatal(int line_num, fmt::format_string<T...> src_file, fmt::format_string<T...> format, T&&... args)
 			{
-				get_InstanceIsoLogger().IsoLog(line_num, src_file, IELogger_Priority::FATAL, "[FATAL]: ", format, fmt::make_format_args(args...));
+				get_InstanceIsoLogger().IsoLog(line_num, src_file, IELogger_Priority::FATAL, "[FATAL]:", format, fmt::make_format_args(args...));
 			}
 
 		};
